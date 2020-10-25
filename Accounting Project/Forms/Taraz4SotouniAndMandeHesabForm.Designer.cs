79796f3d86dac1e26a3ab0e23747d4223f6809ff
Mahyar Golbaz sourkhani <mahyar.golbaz@gmail.com>
2020-10-26 00:47:58 +0330

    Accounting Project

---
+++ Accounting Project/Forms/Taraz4SotouniAndMandeHesabForm.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Accounting_Project
+{
+    partial class Taraz4Sotouni
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Taraz4Sotouni
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Taraz4Sotouni";
+            this.Text = "فرم تراز چهار ستونی و مانده حساب";
+            this.Load += new System.EventHandler(this.Taraz4Sotouni_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
